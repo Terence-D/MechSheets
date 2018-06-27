5f86b8bb304cf081bcba1a4bf23155cb53da9ef8
terence <terence@coffeeshopstudio.ca>
2018-06-27 17:32:34 -0600

    License URL Cleanup

--- app/src/test/java/ca/coffeeshopstudio/meksheets/ExampleUnitTest.java
+++ app/src/test/java/ca/coffeeshopstudio/meksheets/ExampleUnitTest.java
@@ -9,7 +9,7 @@ package ca.coffeeshopstudio.meksheets;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
